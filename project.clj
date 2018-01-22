9e639665fc1c1dbdf24d9b2534b7bf3cea06a1ae
Jereme Corrado <jereme@zoion.net>
2018-01-21 21:57:53 -0500

    Added (text-match) macro tests

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.3.465"]]
-  :plugins [[lein-cljfmt "0.5.7"]])
+                 [org.clojure/core.async "0.3.465"]])
--- test/slackbot_router/core_test.clj
+++ test/slackbot_router/core_test.clj
@@ -1,26 +1,41 @@
 (ns slackbot-router.core-test
   (:require [clojure.test :refer :all]
-            [slackbot-router.core :refer :all]))
+            [slackbot-router.core :refer :all]
+            [slackbot-router.util :refer :all]))
 
 (def test-table-0
   [[(fn [m]
-      (if (= m "match-table-0-test-0")
+      (if (= (:text m) "table-0-test-0")
         m))
     (fn [m] m)]
 
    [(fn [m]
-      (if (= m "match-table-0-test-1")
+      (if (= (:text m) "table-0-test-1")
         m))
-    (fn [m] m)]])
+    (fn [m] m)]
+
+   (text-match "table-0-test-2" "table-0-test-2")
+
+   (text-match #"table-0-test-3" "table-0-test-3")])
 
 (deftest test-root-table-matches
   (testing "A match in the root table"
-    (is (= (route-message test-table-0 "match-table-0-test-0")
-           "match-table-0-test-0")))
+    (let [msg {:text "table-0-test-0"}]
+      (is (= (route-message test-table-0 msg) msg))))
 
   (testing "A miss in the root table"
-    (is (nil? (route-message test-table-0 "should-not-match"))))
+    (let [msg {:text "should-not-match"}]
+      (is (nil? (route-message test-table-0 msg)))))
 
   (testing "A match in the root table, skipping a prior miss"
-    (is (= (route-message test-table-0 "match-table-0-test-1")
-           "match-table-0-test-1"))))
+    (let [msg {:text "table-0-test-1"}]
+      (is (= (route-message test-table-0 msg) msg)))))
+
+(deftest test-text-match-macro
+  (testing "text-match macro with string"
+    (let [msg {:text "table-0-test-2"}]
+      (is (= (route-message test-table-0 msg) msg))))
+
+  (testing "text-match macro with regex"
+    (let [msg {:text "table-0-test-3"}]
+      (is (= (route-message test-table-0 msg) msg)))))
